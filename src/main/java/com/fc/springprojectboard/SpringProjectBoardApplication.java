1dbedd0dcf6d5e4251c0ecf77903efde23b088ba
hyeseong-dev <hyeseong43@gmail.com>
2024-05-28 12:08:08 +0900

    #6 - 스프링부트 시작
    스프링 부트 시작
    초기 기술 :
    * 엑츄에이터
    * 데브툴즈
    * 롬복
    * 스프링웹

---
+++ src/main/java/com/fc/springprojectboard/SpringProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fc.springprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc/springprojectboard/SpringProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.springprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
